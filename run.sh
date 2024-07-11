fb7510f6d3d56b8cf1d7670d566d70f3e22e52c7
Amith Koujalgi <amith.koujalgi@razorthink.com>
2024-07-11 14:43:50 +0530

    init

--- run.sh
+++ run.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 docker run \
-  --gpus=all -it \
+  --gpus=all \
+  -it \
   -p 8883:8070 \
   -p 8884:9001 \
   -p 8885:8885 \
   -p 8886:8886 \
   -p 8887:8887 \
   -p 8888:8888 \
-  ~/llama3-playground-data/:/app/data \
+  ~/llama3-playground-data:/app/data \
   llama3-finetuning:0.1
+
+
+#docker run \
+#  --gpus=all \
+#  -it \
+#  -p 8883:8070 \
+#  -p 8884:9001 \
+#  -p 8885:8885 \
+#  -p 8886:8886 \
+#  -p 8887:8887 \
+#  -p 8888:8888 \
+#  llama3-finetuning:0.1
